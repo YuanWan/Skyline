298ef4373ecfb0ecb8e4957a362094f714b4982a
Wan Yuan <wanyuansg@163.com>
2015-12-22 13:09:42 +0800

    init

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+from bson import json_util
+from bson.json_util import dumps
+
+app = Flask(__name__)
+
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'test'
+# COLLECTION_NAME = 'projects'
+# FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'date_posted': True, 'total_donations': True, '_id': False}
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/stats")
+def stats():
+    return render_template("stats.html")
+
+
+@app.route("/db/twitter")
+def db_twitter():
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME]['twitter']
+    twitters = collection.find(limit=10000)
+    #projects = collection.find(projection=FIELDS)
+    json_twitters = []
+    for twitter in twitters:
+        json_twitters.append(twitter)
+    json_twitters = json.dumps(json_twitters, default=json_util.default)
+    connection.close()
+    return json_twitters
+
+@app.route("/db/news")
+def db_news():
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME]['news']
+    news = collection.find(limit=100)
+    #projects = collection.find(projection=FIELDS)
+    json_news = []
+    for new in news:
+        json_news.append(new)
+    json_news = json.dumps(json_news, default=json_util.default)
+    connection.close()
+    return json_news
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
