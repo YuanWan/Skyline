69246445034410673a9e996f3ceee56d0f9059e1
Wan Yuan <wanyuansg@163.com>
2016-01-03 17:28:06 +0800

    manual msg pool

--- app.py
+++ app.py
@@ -7,19 +7,44 @@ from bson import json_util
 from bson.json_util import dumps
 from flask import request
 import frequentTools
+from flask import session
+from flask.ext.socketio import emit, join_room, leave_room
 from flask_socketio import SocketIO
-import birdy
-from birdy.twitter import UserClient
+import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+msgpool=[]
 
-client = UserClient('Ghy8tbRezuJH0AtSL1qcPjqm8',
-                    'bURQpyYNU7ZbPbZVLiCagInnbM4yZvK9hAFlhToLoZ3XjOCqSy',
-                    '3220384082-tbVafpAHKF2OMyv3m68cUa1xLfTmJ8Riz6bry6l',
-                    'Eb1LiineWIRngaOwju91tEV3cexAcJhvCoxW4dNJ5UQMu')
+
+access_token = "3220384082-tbVafpAHKF2OMyv3m68cUa1xLfTmJ8Riz6bry6l"
+access_token_secret = "Eb1LiineWIRngaOwju91tEV3cexAcJhvCoxW4dNJ5UQMu"
+consumer_key = "Ghy8tbRezuJH0AtSL1qcPjqm8"
+consumer_secret = "bURQpyYNU7ZbPbZVLiCagInnbM4yZvK9hAFlhToLoZ3XjOCqSy"
+
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+        # socketio.emit('message', {'msg': data})
+        # trigger_msg(data)
+        global msgpool
+        msgpool.append(data)
+        print(data)
+        return True
+    def on_error(self, status):
+        print(status)
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
 
 
 MONGODB_HOST = 'localhost'
@@ -28,6 +53,10 @@ DBS_NAME = 'test'
 # COLLECTION_NAME = 'projects'
 # FIELDS = {'school_state': True, 'resource_type': True, 'poverty_level': True, 'time': True, 'total_donations': True, '_id': False}
 connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+
+
+
+
 
 
 @app.route("/")
@@ -39,8 +68,16 @@ def index():
 def stats():
     return render_template("stats.html")
 
-@app.route("/gmonitor")
+@app.route("/sock")
 def gmonitor():
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    global msgpool
+    msgpool = []
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+    streamfire = stream.filter(track=['python', 'javascript', 'ruby'], async=True)
     return render_template("gmonitor.html")
 
 
@@ -76,34 +113,78 @@ def db_news():
     return json_news
 
 
-@socketio.on('message', namespace='/sock')
-def handle_message(message):
-    print('received message: ' + message)
+@app.route("/db/pool")
+def db_pool():
+    global msgpool
+    json_pool = []
+    for msg in msgpool:
+        json_pool.append(msg)
+    json_pool = json.dumps(json_pool, default=json_util.default)
+    msgpool=[]
+    return json_pool
 
 
-@socketio.on('my event', namespace='/sock')
-def handle_my_custom_namespace_event(json):
-    print('received json: ' + str(json))
+# @socketio.on('message', namespace='/sock')
+# def handle_message(message):
+#     print('received message: ' + message)
 
 
-@socketio.on('tracking', namespace='/sock')
-def handle_message(tracking):
-    response = client.stream.statuses.filter.post(track='twitter')
-    for data in response.stream():
-        socketio.emit('my response', data, broadcast=True)
+# @socketio.on('my event', namespace='/sock')
+# def handle_my_custom_namespace_event(json):
+#     print('received json: ' + str(json))
 
 
-@socketio.on('connect', namespace='/sock')
-def test_connect():
-    response = client.stream.statuses.filter.post(track='twitter')
-    for data in response.stream():
-        socketio.emit('my response', data, broadcast=True)
 
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
+# @socketio.on('connect', namespace='/sock')
+# def test_connect():
+#     socketio.emit('listening',{})
+
+#
+# @socketio.on('tracking', namespace='/sock')
+# def tracking():
+#     socketio.emit('my response','sdfsdfsdf')
+#
+#
+# @socketio.on('disconnect', namespace='/test')
+# def test_disconnect():
+#     print('Client disconnected')
+
+
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    # room = session.get('room')
+    # join_room(room)
+    emit('status', {'msg': ' has entered the room.'})
+
+
+@socketio.on('text', namespace='/chat')
+def sock(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    # room = session.get('room')
+    emit('message', {'msg': ':' + message['msg']})
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    """Sent by clients when they leave a room.
+    # A status message is broadcast to all people in the room."""
+    # room = session.get('room')
+    # leave_room(room)
+    emit('status', {'msg':  ' has left the room.'})
+
+
+def trigger_msg(msg):
+    sock(msg)
+
 
 if __name__ == "__main__":
+    # from gevent import monkey
+    # monkey.patch_all()
+    # stream = Stream(auth, l)
+    # streamfire = stream.filter(track=['python', 'javascript', 'ruby'], async=True)
     socketio.run(app)
     app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ static/js/gapp.js
@@ -0,0 +1,183 @@
+$(document).ready(function(){
+            var data=[];
+            var map = L.map('map').setView([19.481924, 19.049447], 3);
+
+            L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-cnkhv76j/{z}/{x}/{y}.png', {
+                    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+                    maxZoom: 18
+            }).addTo(map);
+
+             function getColor(d) {
+                return d > 0.8  ? '#1A6A34' :
+                       d > 0.6  ? '#8CBD31' :
+                       d > 0.2  ? '#FEDE00' :
+                       d > -0.2  ? '#FFDB4E' :
+                       d > -0.6   ? '#E88A3C' :
+                       d > -0.8   ? '#FF4A47' :
+                       '#FF4A47' ;                                  
+            };
+
+            // Set the dimensions of the canvas / graph
+            var margin = {top: 30, right: 20, bottom: 30, left: 30},
+                width = 500 - margin.left - margin.right,
+                height = 250 - margin.top - margin.bottom;
+
+            // Parse the date / time
+            var parseDate = d3.time.format("%d-%b-%y").parse;
+                        
+            // Set the ranges
+            var x = d3.scale.linear().range([0, width]);
+            var y = d3.scale.linear().range([height, 0]);
+
+            var yMin = d3.min(data);
+            var yMax = d3.max(data);
+
+            // Define the axes
+            var xAxis = d3.svg.axis().scale(x)
+                .orient("bottom").ticks(5);
+
+            var yAxis = d3.svg.axis().scale(y)
+                .orient("left").ticks(5);
+
+            // Define the line
+            var valueline = d3.svg.line()
+                .x(function(d,i) { return x(i); })
+                .y(function(d) { return y(d); });
+                
+            // Adds the svg canvas
+            var svg = d3.select("#chart")
+                .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                    .attr("transform", 
+                          "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+            // Scale the range of the data
+            x.domain(d3.extent(data, function(d,i) { return i ; }));
+            y.domain([0, d3.max(data, function(d) { return d })]);
+
+            // Add the valueline path.
+            svg.append("path")
+                .attr("class", "line")
+                .attr("d", valueline(data));                
+
+            // Add the X Axis
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis)
+                .append("text")
+                  .attr("x", width)
+                  .attr("y", -18)
+                  .attr("dy", ".90em")
+                  .style("text-anchor", "end")
+                  .text("Seconds");
+
+            // Add the Y Axis
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                  .attr("transform", "rotate(-90)")
+                  .attr("y", 6)
+                  .attr("dy", ".71em")
+                  .style("text-anchor", "end")
+                  .text("Tweets");
+
+
+
+            //namespace = '/gmonitor'; // change to an empty string to use the global namespace
+
+            // the socket.io documentation recommends sending an explicit package upon connection
+            // this is specially important when using the global namespace
+            var socket = io.connect('http://' + document.domain + ':' + location.port + '/chat');
+
+            socket.on('connect', function() {
+                    socket.emit('joined', {});
+                });
+            socket.on('status', function(data) {
+                    //$('#chat').val($('#chat').val() + '<' + data.msg + '>\n');
+                    //$('#chat').scrollTop($('#chat')[0].scrollHeight);
+                });
+            socket.on('message', function(data) {
+                    $('#chat').val($('#chat').val() + data.msg + '\n');
+                    $('#chat').scrollTop($('#chat')[0].scrollHeight);
+                });
+
+
+            //connect
+            //socket.on('connect', function() {
+            //    socket.emit('tracking', {});
+            //});
+
+            // event handler for server sent data
+            // the data is displayed in the "Received" section of the page
+            //socket.on('my response', function(msg) {
+            //    $('#log').prepend('<br>Received #' + msg.count + ': ' + msg.status);
+            //});
+            //
+            //socket.on('error', function(msg) {
+            //    $('#log').append('<br>Received #' + msg.count + ': ' + msg);
+            //});
+            
+            socket.on('message', function(msg) {
+                //console.log(msg);
+                $('#count').html(msg.count);
+                if (typeof(msg.speed) != 'undefined') {
+                    $('#speed').html(msg.speed);
+                    //console.log(msg);
+                    updateData(msg.speed);
+                    $(".subjetivity").animate({width: msg.subjetivity_avg*100+"%"});    
+                    $(".polarity").animate({width: msg.polarity_avg*100+"%"});                        
+                                        
+                };
+                
+                
+                var geojsonMarkerOptions = {
+                radius: 3,
+                fillColor: getColor(msg.polarity),
+                color: "#FFBE08",
+                weight: 1,
+                opacity: 0.1,
+                fillOpacity: 0.5
+                };
+                L.circleMarker(msg.coordinates,geojsonMarkerOptions)
+                 .addTo(map).bindPopup("<div class='row'><div class='col-md-3'><img src="+msg.profile_image+" class='img-rounded'></div><div class='col-md-9'><h5 style='color:black'>"+msg.username+"</h5><p style='color:grey'>"+msg.status+"</p></div></div>");   
+                               
+            });    
+
+
+            function updateData(speed) {
+
+            // Get the data again
+            
+            data.push(speed);
+
+            // Scale the range of the data again 
+            x.domain(d3.extent(data, function(d,i) { return i; }));
+            y.domain([0, d3.max(data, function(d) { return d; })]);
+
+            // Select the section we want to apply our changes to
+            var svg = d3.select("#chart").transition();
+
+            // Make the changes
+                svg.select(".line")   // change the line
+                    .duration(200)
+                    .attr("d", valueline(data));
+                svg.select(".x.axis") // change the x axis
+                    .duration(200)
+                    .call(xAxis);
+                svg.select(".y.axis") // change the y axis
+                    .duration(200)
+                    .call(yAxis);
+
+            
+        };         
+            
+
+       
+
+ });
