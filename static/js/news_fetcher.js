21aa68368fade02d11ebfe683d3997d9cd45f333
Wan Yuan <wanyuansg@163.com>
2016-01-19 22:21:49 +0800

    newspaper layout

--- static/js/news_fetcher.js
+++ static/js/news_fetcher.js
@@ -11,16 +11,21 @@ var newsAPI = "/db/news";
   })
     .done(function( data ) {
       $.each( data, function( i, news ) {
-          var anchor = "<a href="+news.url+">";
+          var article_url = "<a href="+news.url+">";
           var a_end = "</a>";
           var panel='<div class="panel panel-primary">';
           var panel_heading='<div class="panel-heading">';
-          var panel_body='<div class="panel-body">'
-          var title = "<h3 class='panel-title'>"+ news.title +"";
+          var panel_body='<div class="panel-body">';
+          var title = '<h3 class="panel-title">'+ news.title +"</h3>";
           var text= news.text.substr(0,500);
           var div_end = '</div>';
+          var keywords="";
+          if(news.keywords[0]!=""){
+              keywords="<div class='panel-footer'>"+"Keywords: "+news.keywords+"</div>";
+          }
+          var source="<div class='panel-footer'> Source: <a href="+news.source+">"+news.source+"</a></div>";
           $("#news_list").prepend(
-              anchor+panel+panel_heading+title+div_end+panel_body+text+div_end+div_end+a_end
+              panel+panel_heading+title+div_end+article_url+panel_body+text+div_end+a_end+keywords+source+div_end
           )
       });
     });
